dc3acd97f4cc193ea01e08d6b32482ce559de9c2
Veer2912 <choudharyveer297@gmail.com>
2024-07-12 16:22:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const browser = await puppeteer.launch({headless:false});
 const page = await browser.newPage();
 await page.setViewport({width: 1080, height: 1024});
 await page.goto("https://www.calculator.net/");
-// await page.screenshot({path: "calculator.png"});
-await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(2)').click();
-await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(3)').click();
-await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(4)').click();
-await page.locator('td:nth-of-type(2) div:nth-of-type(3) > span:nth-of-type(3)').click();
-await page.locator('td:nth-of-type(2) div:nth-of-type(3) > span:nth-of-type(2)').click();
+await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(2)').click({delay:1000});
+await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(3)').click({delay:1000});
+await page.locator('td:nth-of-type(2) div:nth-of-type(1) > span:nth-of-type(4)').click({delay:1000});
+await page.locator('td:nth-of-type(2) div:nth-of-type(3) > span:nth-of-type(3)').click({delay:1000});
+await page.locator('td:nth-of-type(2) div:nth-of-type(3) > span:nth-of-type(2)').click({delay:1000});
 await page.waitForSelector('#sciOutPut');
 const result = await page.evaluate(() => {
     return document.querySelector('#sciOutPut').textContent;
 });
+// await page.screenshot({path: "calculator.png"});
 console.log(`result:${result}`);
-await browser.waitForTarget(()=> false);
+await browser.waitForTarget(()=> false,);
 await browser.close()
 }) ();
